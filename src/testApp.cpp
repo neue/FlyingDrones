e79e05334d8c045818ea8338d0f2d1a9dea34111
James Hicks <james@dieneuemodern.com>
2013-03-03 21:38:04 +0000

    Clouds

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -12,7 +12,7 @@ void testApp::setup(){
 	droneRenderer->loadTexture("DronesSpritesheet.png", 1024, GL_NEAREST);
     spriteAdded = false;
     timeCode = 0.0;
-
+    windSpeed=ofRandom(1,3);
     
 }
 
@@ -31,37 +31,62 @@ void testApp::update(){
     			sprites[i]->pos.y+=(sprites[i]->speed)/2; //add their speed to their y
     			sprites[i]->pos.y += sin(timeCode*20);
     			sprites[i]->pos.x-=sprites[i]->speed; //add their speed to their y
-    
+                
     			if(sprites[i]->pos.y > ofGetHeight()+16)
                     //if they are past the bottom of the screen
     			{
     				sprites[i]->pos.x = ofRandom(100,1800);
      				sprites[i]->pos.y = -50;
-   			} else {
-                        droneRenderer->addCenteredTile(&sprites[i]->animation, sprites[i]->pos.x, sprites[i]->pos.y);
+                } else {
+                    droneRenderer->addCenteredTile(&sprites[i]->animation, sprites[i]->pos.x, sprites[i]->pos.y);
                     // add them to the sprite renderer (add their animation at their position, there are a lot more options for what could happen here, scale, tint, rotation, etc, but the animation, x and y are the only variables that are required)
-    
+                    
                 }
                 
-                        
+                
+    		}
+    		for(int i=cloudSprites.size()-1;i>=0;i--) //go through them
+    		{
+    			cloudSprites[i]->pos.x-=windSpeed; //add their speed to their y
+                
+    			if(cloudSprites[i]->pos.x < 0){
+    				cloudSprites[i]->pos.x = ofGetWidth();
+     				cloudSprites[i]->pos.y = ofRandom(0,800);
+                } else {
+                    droneRenderer->addCenteredTile(&cloudSprites[i]->animation, cloudSprites[i]->pos.x, cloudSprites[i]->pos.y);
+                    // add them to the sprite renderer (add their animation at their position, there are a lot more options for what could happen here, scale, tint, rotation, etc, but the animation, x and y are the only variables that are required)
+                    
+                }
+                
+                
     		}
     	}
 
     
-    	if(spriteAdded == false){
-    		basicSprite * newSprite = new basicSprite(); // create a new sprite
-    		newSprite->pos.set(ofRandom(100,1800),200); //set its position
-    		newSprite->speed=ofRandom(1,5); //set its speed
-            newSprite->sheetRow = 0;
-    		newSprite->animation = flyingAnimation; //set its animation to the walk animation we declared
-    		newSprite->animation.frame_duration /= newSprite->speed; //adjust its frame duration based on how fast it is walking (faster = smaller)
-    		newSprite->animation.index = newSprite->sheetRow*8; //change the start index of our sprite. we have 4 rows of animations and our spritesheet is 8 tiles wide, so our possible start indicies are 0, 8, 16, and 24
-    		sprites.push_back(newSprite); //add our sprite to the vector
-            if (sprites.size() > 20) {
-                spriteAdded = true;
-            }
-            
-    	}
+    if(spriteAdded == false){
+        droneSprite * newSprite = new droneSprite(); // create a new sprite
+        newSprite->pos.set(ofRandom(100,1800),200); //set its position
+        newSprite->speed=ofRandom(1,5); //set its speed
+        newSprite->sheetRow = 0;
+        newSprite->animation = flyingAnimation; //set its animation to the walk animation we declared
+        newSprite->animation.frame_duration /= newSprite->speed; //adjust its frame duration based on how fast it is walking (faster = smaller)
+        newSprite->animation.index = newSprite->sheetRow*8; //change the start index of our sprite. we have 4 rows of animations and our spritesheet is 8 tiles wide, so our possible start indicies are 0, 8, 16, and 24
+        sprites.push_back(newSprite); //add our sprite to the vector
+        if (sprites.size() > 5) {
+            spriteAdded = true;
+        }
+    }
+    if(cloudsAdded == false){
+        cloudSprite * newCloud = new cloudSprite(); // create a new sprite
+        newCloud->pos.set(ofRandom(100,1800),ofRandom(0,800)); //set its position
+        newCloud->offset = ofRandom(0,3);
+        newCloud->animation = cloudAnimation; //set its animation to the walk animation we declared
+        newCloud->animation.index = 7 + newCloud->offset; //change the start index of our sprite. we have 4 rows of animations and our spritesheet is 8 tiles wide, so our possible start indicies are 0, 8, 16, and 24
+        cloudSprites.push_back(newCloud); //add our sprite to the vector
+        if (cloudSprites.size() > 20) {
+            cloudsAdded = true;
+        }
+    }
 
 
 }
--- src/testApp.h
+++ src/testApp.h
@@ -16,13 +16,36 @@ static animation_t flyingAnimation =
 	1	/* .frameskip		(int) - the incrementation of each frame. 1 should be set by default. If you wanted the animation to play backwards you could set this to -1, etc.	*/
 };
 
+static animation_t cloudAnimation =
+{	7,	/* .index			(int) - this is the index of the first animation frame. indicies start at 0 and go left to right, top to bottom by tileWidth on the spriteSheet		*/
+	0,	/* .frame			(int) - this is the current frame. It's an internal variable and should always be set to 0 at init													*/
+	1,	/* .totalframes		(int) - the animation length in frames																												*/
+	1,	/* .width			(int) - the width of each animation frame in tiles																									*/
+	1,	/* .height			(int) - the height of each animation frame in tiles																									*/
+	75,	/* .frameduration	(unsigned int) - how many milliseconds each frame should be on screen. Less = faster																*/
+	0,	/* .nexttick		(unsigned int) - the next time the frame should change. based on frame duration. This is an internal variable and should always be set to 0 at init	*/
+	-1,	/* .loops			(int) - the number of loops to run. -1 equals infinite. This can be adjusted at runtime to pause animations, etc.									*/
+	-1,	/* .finalindex		(int) - the index of the final tile to draw when the animation finishes. -1 is the default so total_frames-1 will be the last frame.				*/
+	1	/* .frameskip		(int) - the incrementation of each frame. 1 should be set by default. If you wanted the animation to play backwards you could set this to -1, etc.	*/
+};
 
-//a quick and dirty sprite implementation
-struct basicSprite {
+
+struct droneSprite {
 	animation_t animation;	// a variable to store the animation this sprite uses
 	ofPoint pos;			// the position on the screen this sprite will be drawn at
 	float speed;			// the speed at which this sprite moves down the screen
     int sheetRow;
+};
+
+
+//
+// CLOUDS
+//
+
+struct cloudSprite {
+	animation_t animation;
+	ofPoint pos;			
+    int offset;
 };
 
 
@@ -43,11 +66,13 @@ class testApp : public ofBaseApp{
 		void gotMessage(ofMessage msg);
     
         ofxSpriteSheetRenderer * droneRenderer;
-        vector<basicSprite *> sprites;				// our vector of sprites
+        vector<droneSprite *> sprites; // our vector of sprites
+        vector<cloudSprite *> cloudSprites; // our vector of sprites
 
         bool spriteAdded;
+        bool cloudsAdded;
         ofImage rpiLogo;
-    
+        int windSpeed;
         int angle;
         float timeCode;
 
